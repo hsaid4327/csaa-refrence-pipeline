b897ce8ff5e17735338dd3eef3aee75d1819d6d9
Hammad Said <hsaid@hsaid.remote.csb>
2021-06-02 17:08:38 -0700

    test

---
+++ app-src/buildMaven.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+  
+ pomFile="pom.xml"
+
+  if [[ ! -a $pomFile ]]; 
+    then
+  echo "The file does not exist"
+  echo "$0: must be run from the root directory of the application"
+   exit 1
+  fi 
+ 
+ if [[ $# -lt 1 ]];
+   then
+ echo "The version number for the build must be provided. $0 version [example: $0 1.1]"
+ exit 1
+ fi
+ version=$1
+
+
+
+ mvn versions:set -DnewVersion=$version
+
+ mvn clean install
